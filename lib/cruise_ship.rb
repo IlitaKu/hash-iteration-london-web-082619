248d0b3a2a31c7943e9628632603a9d26f7de8a6
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-23 19:52:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,3 +18,8 @@ end
 def happy_birthday(birthday_kids)
   birthday_kids.map { |kids_name,age| "Happy Birthday #{kids_name}! You are now #{age} years old!"}
 end
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
