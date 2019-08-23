bfd40844da9ce029fd36aba8e35bc115a0742b0a
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-23 20:06:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@
   birthday_kids.map { |kids_name,age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
 end
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_age, name|
+  birthday_kids.map do |kids_age, name|
     if name <= 12
       puts "Happy Birthday #{kids_age}! You are now #{name} years old!"
     else 
